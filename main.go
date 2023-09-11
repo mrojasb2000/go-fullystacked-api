78ca71d01411b6dd4a4fca4a27cbb18efed46774
maalroja <maalroja@bancolombia.com.co>
2023-09-11 13:06:34 -0300

    chore: add handler alternative

--- main.go
+++ main.go
@@ -15,6 +15,17 @@ func handlerGetHelloWorld(w http.ResponseWriter, req *http.Request) {
 	log.Println(req.Body)
 }
 
+type foo struct {
+}
+
+func (d foo) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Hello World Alternative\n")
+	log.Println(req.Method)
+	log.Println(req.URL)
+	log.Println(req.Header)
+	log.Println(req.Body)
+}
+
 func main() {
 	port := "9002"
 	router := http.NewServeMux()
@@ -27,10 +38,17 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 
+	// This is just to show an alternative way to declare a handler
+	// by having a struct that implements the ServeHTTP(...) interface
+	dummyHandler := foo{}
+
 	router.HandleFunc("/", handlerGetHelloWorld)
+
+	router.Handle("/1", dummyHandler)
 
 	err := srv.ListenAndServe()
 	if err != nil {
 		log.Fatalln("Couldn't Listen and Server: ", err)
 	}
+	log.Println("Server is running on port :", port)
 }
