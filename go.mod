3d7ad09652b2d547cd2cbfbbeca947980a92201d
maalroja <maalroja@bancolombia.com.co>
2023-09-11 16:25:41 -0300

    feat: add Gorilla Multiplex

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mrojasb2000/go-fullystacked-api
 
 go 1.21.1
 
-require github.com/lib/pq v1.10.9 // indirect
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/lib/pq v1.10.9 // indirect
+)
--- main.go
+++ main.go
@@ -1,54 +1,90 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
-	"time"
+	"os"
+
+	"github.com/gorilla/mux"
 )
 
-func handlerGetHelloWorld(w http.ResponseWriter, req *http.Request) {
+func HandlerSlug(w http.ResponseWriter, r *http.Request) {
+	slug := mux.Vars(r)["slug"]
+	if slug == "" {
+		log.Println("Slug not provided")
+		return
+	}
+	log.Println("Got slug, slug")
+}
+
+func handlerGetHelloWorld(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World\n")
-	log.Println(req.Method)
-	log.Println(req.URL)
-	log.Println(req.Header)
-	log.Println(req.Body)
+	log.Println("Request via:\t", r.Method)
+	log.Println("Request url:\t", r.URL)
+	log.Println("Request Header:\t", r.Header)
+	log.Println("Request body:\t", r.Body)
 }
 
-type foo struct {
-}
+func handlerPostEcho(w http.ResponseWriter, r *http.Request) {
+	log.Println("Request via:\t", r.Method)
+	log.Println("Request url:\t", r.URL)
+	log.Println("Request Header:\t", r.Header)
 
-func (d foo) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "Hello World Alternative\n")
-	log.Println(req.Method)
-	log.Println(req.URL)
-	log.Println(req.Header)
-	log.Println(req.Body)
+	// We are going to read it into a buffer
+	// as the request body is an io.ReadCloser
+	// and so we should only read it once.
+	body, _ := io.ReadAll(r.Body)
+	log.Println("read >t", string(body), "\t<")
+
+	n, err := io.Copy(w, bytes.NewReader(body))
+	if err != nil {
+		log.Println("Error echoing response: ", err)
+	}
+	log.Println("Wrote back ", n, " bytes")
 }
 
 func main() {
+	// Set some flags for easy debugging
+	log.SetFlags(log.Lshortfile | log.Ldate | log.Lmicroseconds)
+
+	// Get a port from ENV var or default to 9002
 	port := "9002"
-	router := http.NewServeMux()
+	if value, exists := os.LookupEnv("SERVER_PORT"); exists {
+		port = value
+	}
+
+	// Off the back, we can enforce StrictSlash
+	// This is a nice helper function that means
+	// When true, if the route is "/foo/",
+	// accessing "/foo" will peform a 301
+	// redirect to the former and vice versa.
+	// In other words, your application will
+	// always see the path as specified in the
+	// route.
+	// When false, if the route path is "/foo"
+	// accessing "/foo/" will not math this
+	// route and vice versa.
+	router := mux.NewRouter().StrictSlash(true)
 
 	srv := http.Server{
-		Addr:           ":" + port,
-		Handler:        router,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   120 * time.Second,
-		MaxHeaderBytes: 1 << 20,
+		Addr: ":" + port,
+		// Addr optionally
+		// specifies the listen address for the
+		// server in the form of "host:port".
+		Handler: router,
 	}
 
-	// This is just to show an alternative way to declare a handler
-	// by having a struct that implements the ServeHTTP(...) interface
-	dummyHandler := foo{}
+	router.HandleFunc("/", handlerGetHelloWorld).Methods(http.MethodGet)
+	router.HandleFunc("/", handlerPostEcho).Methods(http.MethodPost)
+	router.HandleFunc("/{slug}", HandlerSlug).Methods(http.MethodGet)
 
-	router.HandleFunc("/", handlerGetHelloWorld)
-
-	router.Handle("/1", dummyHandler)
-
+	log.Println("Starting on port: ", port)
 	err := srv.ListenAndServe()
 	if err != nil {
-		log.Fatalln("Couldn't Listen and Server: ", err)
+		log.Fatalln("Counldn't Listen and Serve :", err)
 	}
-	log.Println("Server is running on port :", port)
+	fmt.Println("Server is running on port: ", port)
 }
